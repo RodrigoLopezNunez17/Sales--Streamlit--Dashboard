3f451aae79558d8f6c3a1f22d7193971cfccab6f
IMNOTROY <roylopnun@gmail.com>
2024-08-01 00:06:16 -0600

    Strealit Pages

---
+++ pages/page2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Page 2",
+    page_icon="🦈",
+    layout="wide"
+)
+
+st.title("This is the page number 2.")
---
+++ pages/page3.py
@@ -0,0 +1,9 @@
+import pandas as pd, streamlit as st
+
+st.set_page_config(
+    page_title="Page 3",
+    page_icon="🦈",
+    layout='wide'
+)
+
+st.title("This is the page number 3.")
